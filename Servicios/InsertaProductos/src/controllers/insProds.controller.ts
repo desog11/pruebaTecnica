39eb38af76dacd1ee6b97ef5e104e51ed23b337e
desog11 <diegoesog11@gmail.com>
2024-06-20 08:35:02 -0600

    refactor(desarrollo): cambio a servicio insertaProds

--- Servicios/InsertaProductos/src/controllers/insProds.controller.ts
+++ Servicios/InsertaProductos/src/controllers/insProds.controller.ts
@@ -2,7 +2,7 @@ import {Request,Response} from 'express'
 import connection from '../db/connection'
 
 export const insProds = (req:Request,res:Response)=>{
-    connection.query('CALL sp_maneja_eventos(\'IPN\', ?, ?,?, NULL, NULL, NULL, NULL, NULL, NULL, NULL);',[req.body.nombre, req.body.descripcion,req.body.precio]
+    connection.query('CALL sp_maneja_eventos(\'IPS\', ?, ?,?, NULL, NULL, NULL, NULL, NULL, NULL, NULL,?);',[req.body.nombre, req.body.descripcion,req.body.precio,req.body.tipo]
         ,(err,data)=>{
         if(err){
             console.log(err)
@@ -10,8 +10,14 @@ export const insProds = (req:Request,res:Response)=>{
                 error: 'ERROR AL INSERTAR PRODUCTOS INTENTE MÁS TARDE'
             })
         }else{
+            let mensaje =''
+            if(req.body.tipo == 1){
+                mensaje = 'PRODUCTO INSERTADO CON ÉXITO'
+            }else{
+                mensaje = 'SEVICIO INSERTADO CON ÉXITO'
+            }
             res.status(200).json({
-                mensaje: 'PRODUCTO INSERTADO CON ÉXITO'
+                mensaje
             })
         }
     });
