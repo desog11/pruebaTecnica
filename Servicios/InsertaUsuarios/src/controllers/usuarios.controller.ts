68e63ea4c091799eb70253b9991ac0fe2868ed5f
desog11 <diegoesog11@gmail.com>
2024-06-21 00:06:03 -0600

    refactor(desarrollo): cors para inserta usuarios

--- Servicios/InsertaUsuarios/src/controllers/usuarios.controller.ts
+++ Servicios/InsertaUsuarios/src/controllers/usuarios.controller.ts
@@ -2,6 +2,10 @@ import {Request,Response} from 'express'
 import connection from '../db/connection'
 
 export const insertaUsuarios = (req:Request,res:Response)=>{
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.headers.origin && ["http://34.125.179.240:8080/", "http://localhost:8080/", "http://192.168.64.2:8080/","http://0.0.0.0:8080/"].includes(req.headers.origin)) {
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+      }
     let fecha = FechaConvierte(req.body.fecha)
     connection.query('CALL sp_maneja_eventos(\'IUS\', ?, NULL,NULL, ?, NULL, NULL, ?, NULL, NULL, NULL,NULL,?);',[req.body.nombre, fecha,req.body.correo_electronico,req.body.apellido],
         (err,data)=>{
