efa9c9ad24550600fb4c8f5a8eccc8d8dc130006
desog11 <diegoesog11@gmail.com>
2024-06-21 10:19:17 -0600

    feat(desarrollo): incorporación de depends on docker compose

--- FrontEnd/eventos/src/App.tsx
+++ FrontEnd/eventos/src/App.tsx
@@ -10,7 +10,7 @@ const App = (): JSX.Element => {
   const [todos,setTodos] = useState<Productos>();
   const [filteredtodos,setFilteredTodos] = useState<Productos>();
   useEffect(() => {
-    axios.get<Productos>("http://localhost:3001/api/productos").then((response) => {setTodos(response.data); setFilteredTodos(response.data)})
+    axios.get<Productos>("http://backend-obtiene-productos:3001/api/productos").then((response) => {setTodos(response.data); setFilteredTodos(response.data)})
     
   },[]);
   
--- FrontEnd/eventos/src/models/obtieneProductos.ts
+++ FrontEnd/eventos/src/models/obtieneProductos.ts
@@ -1,7 +1,7 @@
 import {Productos} from "../Interfaces/obtieneProds.interface";
 
 async function obtieneProductos(): Promise<Productos[]> {
-    const response = await fetch('http://localhost:3001/api/productos');
+    const response = await fetch('http://backend-obtiene-productos:3001/api/productos');
     const data = await response.json();
     return data;
   }
